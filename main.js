8b6904ab3de48fe5b456d82251bf96714b0ea8bd
brianpr-pr <brian23perezperez>
2025-11-19 11:56:09 +0000

    Created function to add task to the list

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.getElementById('description').addEventListener('input', () => {
         description = document.getElementById('description').value.trim();
-        console.log("Description" , description);
     });
 
     document.getElementById('add-task').addEventListener("click", () => {
@@ -11,7 +10,9 @@ document.addEventListener("DOMContentLoaded", () => {
         
         //If description is not empty we add new task to the list else show error message
         if(description !== ""){
-            console.log(ele);
+            if(contentTask.querySelector(".error-message") !== null)
+                contentTask.removeChild( contentTask.querySelector(".error-message") );
+            addTask(description);
         } else{
             if(contentTask.querySelector(".error-message") === null)
                 contentTask.appendChild(createErrorMessage("Error Message"));
@@ -28,4 +29,31 @@ function createErrorMessage(errorMessage){
     ele.style.color = "red";
     ele.classList.add('error-message');
     return ele;
+}
+
+function addTask(description){
+    let taskList = document.getElementById('task-list');
+
+    let liElement = document.createElement("li");
+    taskList.appendChild(liElement);
+    let divElement = document.createElement("div");
+    divElement.classList.add("task-item", "d-flex-column");
+    liElement.appendChild(divElement);
+
+
+    let h4Element = document.createElement("h4");
+    h4Element.style.textAlign = "center";
+    let h4Content = document.createTextNode("Description");
+    h4Element.appendChild(h4Content);
+    divElement.appendChild(h4Element);
+
+    let pElement = document.createElement("p");
+    let pContent = document.createTextNode(description);
+    pElement.appendChild(pContent);
+    divElement.appendChild(pElement);
+
+    let buttonElement = document.createElement("button");
+    let buttonContent = document.createTextNode("Delete Task");
+    buttonElement.appendChild(buttonContent);
+    divElement.appendChild(buttonElement);
 }
